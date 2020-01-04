d9184237dae39e7642e8eec0d400eafc87f120b0
tong21 <2528941961.qq.com>
2020-01-04 17:45:09 +0800

    清除

--- movies/user/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from user import models
-# Register your models here.
--- movies/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-def register(request):
-    '''
-    注册页
-    ：parm request
-    :return:
-    '''
-    return render(request,'register.html')
-    
